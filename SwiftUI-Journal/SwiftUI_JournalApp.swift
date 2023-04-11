85dbebf1d7e2659f1d5a61ef3b43a85c7ea4d845
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-11 19:59:09 +0200

    Initial Commit

---
+++ SwiftUI-Journal/SwiftUI_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JournalApp.swift
+//  SwiftUI-Journal
+//
+//  Created by David Salmberg on 2023-04-11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
